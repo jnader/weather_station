ecf1b794a3434886f80f9b647d2eaac08f5086cb
Joudy NADER <joudy_nader@hotmail.com>
2025-12-29 17:44:02 +0100

    [DEV JNC-38] Add probability of precipitation

--- weather.py
+++ weather.py
@@ -253,6 +253,7 @@ class Weather:
             weather_data=WeatherMainData.from_dict(data.get("main", None)),
             wind=Wind.from_dict(data.get("wind", None)),
             visibility=data.get("visibility", None),
+            pop=data.get("pop", None),
             clouds=Cloud.from_dict(data.get("clouds", None)),
             rain=Precipitation(data.get("rain", None)),
             snow=Precipitation(data.get("snow", None)),
